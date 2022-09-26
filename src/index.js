52f3e84a444716b86330a208fae30d70610b1ff1
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-09-26 13:13:03 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
-const debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
@@ -8,46 +8,32 @@ const searchQuery = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-searchQuery.addEventListener(
-  'input',
-  debounce(renderSearchQuery, DEBOUNCE_DELAY)
-);
+searchQuery.addEventListener('input',debounce(renderSearchQuery, DEBOUNCE_DELAY));
 
 function renderSearchQuery() {
   const searchQueryValue = searchQuery.value.trim();
   clearCountryRender();
-  checkSearchQueryValue(searchQueryValue)
+  if (searchQueryValue !== '') {
+    fetchCountries(searchQueryValue).then(checkResolveValue);
+  }
 }
 function clearCountryRender() {
   countryList.textContent = '';
   countryInfo.textContent = '';
 }
-function checkSearchQueryValue(searchQueryValue) {
-    if (searchQueryValue !== '') {
-        fetchCountries(searchQueryValue).then(resolve => {
-          checkResolveValue(resolve);
-          renderMarkupAfterCheck(resolve);
-        });
-      }
-}
+
 function checkResolveValue(resolveData) {
   if (resolveData.length > 10) {
-    return Notify.info(
-      'Too many matches found. Please enter a more specific name.'
-    );
-  }
-  if (resolveData.length === 0) {
-    return Notify.failure('Oops, there is no country with that name');
-  }
-}
-function renderMarkupAfterCheck(resolve) {
-  if (resolve.length >= 2 && resolve.length <= 10) {
-    return renderCountryList(resolve);
-  }
-  if (resolve.length === 1) {
-    return renderOneCountry(resolve);
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (resolveData.length === 0) {
+    Notify.failure('Oops, there is no country with that name');
+  } else if (resolveData.length >= 2 && resolveData.length <= 10) {
+    renderCountryList(resolveData);
+  } else {
+    renderOneCountry(resolveData);
   }
 }
+
 function renderCountryList(countries) {
   const markup = countries
     .map(country => {
